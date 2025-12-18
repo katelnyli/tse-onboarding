199b0287e5cbc10c0d973237306abcad9d309401
katelyn <katelynlitree@gmail.com>
2025-12-17 18:27:30 -0800

    Implement part 1.1

---
+++ frontend/src/components/TaskItem.tsx
@@ -0,0 +1,24 @@
+import { CheckButton } from "src/components";
+import styles from "src/components/TaskItem.module.css";
+
+import type { Task } from "src/api/tasks";
+export type TaskItemProps = {
+  task: Task;
+};
+
+export function TaskItem({ task }: TaskItemProps) {
+  let textContainer = styles.textContainer;
+  if (task.isChecked) {
+    textContainer += ` ${styles.checked}`;
+  }
+
+  return (
+    <div className={styles.item}>
+      <CheckButton checked={task.isChecked} />
+      <div className={textContainer}>
+        <span className={styles.title}>{task.title}</span>
+        {task.description && <span className={styles.description}>{task.description}</span>}
+      </div>
+    </div>
+  );
+}
